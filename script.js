a9a0a754fddba71814c0149b605cf2b2b2db6326
Geovanna Otoni <geovannaotoni@gmail.com>
2023-02-08 19:00:17 -0300

    estilizacoes

--- script.js
+++ script.js
@@ -138,4 +138,7 @@ moveDownButton.addEventListener('click', moveDown);
 
 window.onload = () => {
   recoverTasks();
+  
+  //adicionei depois para remover a seleção ao atualizar a página
+  document.getElementsByClassName('selected')[0].classList.remove('selected');
 };
